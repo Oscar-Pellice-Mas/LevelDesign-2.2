981d1fd0b87ab937c511481257ee2307d240f89a
Aileck <61742408+Aileck@users.noreply.github.com>
2024-01-27 21:52:36 +0100

    update particle

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -8,9 +8,12 @@ public class DoorController : MonoBehaviour
     public int RightLimit = 5;
 
     public GameObject LeftDoor;
-    public GameObject RightDoor;
+    public GameObject RightDoor;    
+    
+    public GameObject LeftParticle;
+    public GameObject RightParticle;
 
-    public float doorSpeed = 0.2f;
+    public float doorSpeed = 0.1f;
 
     private float leftDoorTranslationAmount = 0f;
     private float rightDoorTranslationAmount = 0f;
@@ -23,14 +26,18 @@ public class DoorController : MonoBehaviour
     void Update()
     {
         if (SceneFlag.Instance.TryToOpenDoor) {
+            LeftParticle.SetActive(true);
+            RightParticle.SetActive(true);
             if (leftDoorTranslationAmount <= LeftLimit)
             {
+                LeftParticle.transform.Translate(Vector3.left * doorSpeed * Time.deltaTime);
                 LeftDoor.transform.Translate(Vector3.left * doorSpeed * Time.deltaTime);
                 leftDoorTranslationAmount += doorSpeed * Time.deltaTime;
             }
 
             if (rightDoorTranslationAmount <= RightLimit)
             {
+                RightParticle.transform.Translate(Vector3.right * doorSpeed * Time.deltaTime);
                 RightDoor.transform.Translate(Vector3.right * doorSpeed * Time.deltaTime);
                 rightDoorTranslationAmount += doorSpeed * Time.deltaTime;
             }
